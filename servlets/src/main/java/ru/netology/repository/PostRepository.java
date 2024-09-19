34fe094887c16b55e5496b8c75e793f43b0c623a
Vitaliy Murzin <murzinvit@yandex.ru>
2024-09-20 02:51:03 +0500

    change List to Map

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -2,49 +2,37 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 // Stub
 public class PostRepository {
-    List<Post> list = Collections.synchronizedList(new ArrayList<Post>());
-    long count = list.size();
+    Map<Long, Post> map = Collections.synchronizedMap(new HashMap<>());
+    long count = map.size();
 
-    public List<Post> all() {
-        return list;
+    public  Map<Long, Post> all() {
+        return map;
     }
 
     public Optional<Post> getById(long id) {
-        for (Post i : list) {
-            if (i.getId() == id) {
-                return Optional.of(i);
-            }
-            ;
+        if(map.containsKey(id)){
+            return Optional.of(map.get(id));
         }
         return Optional.empty();
     }
 
     public Post save(Post post) {
-        for (int i = 0; i < list.size(); i++) {
-            Post tmp_post = list.get(i);
-            if (post.getId() == tmp_post.getId()) {
-                list.set(i, post);
-                return list.get(i);
-            }
+        if(map.containsKey(post.getId())) {
+            map.put(post.getId(), new Post(post.getId(), post.getContent()));
+            return map.get(post.getId());
         }
-        ;
-        list.add(new Post(count++, post.getContent()));
-        return list.get(list.size() - 1);
+        long tmpcount = count++;
+        map.put(tmpcount, new Post(tmpcount, post.getContent()));
+        return map.get(tmpcount);
     }
 
     public void removeById(long id) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getId() == id) {
-                list.remove(i);
-                return;
-            }
+        if(map.containsKey(id)){
+            map.remove(id);
         }
     }
 }
